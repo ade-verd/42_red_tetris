090c1eacf710227397973ff21f88a12199a97c7b
ADV <ade-verd@student.42.fr>
2020-02-04 17:32:23 +0100

    [ade-verd] finish tests of createNewRandomTetriminos()

--- test/server/lib/tetriminos/index.test.js
+++ test/server/lib/tetriminos/index.test.js
@@ -15,7 +15,7 @@ describe('lib/tetriminos', () => {
 		sandbox.restore();
 	});
 
-	describe.skip('#_getRandomRotation()', () => {
+	describe('#_getRandomRotation()', () => {
 		it('should return the same tetrimino if there is only one rotation possible', async () => {
 			const res = await tetriminosLib.__TESTS__._getRandomRotation(fixtures.tetriminos().O);
 
@@ -46,23 +46,6 @@ describe('lib/tetriminos', () => {
 			});
 		});
 
-		it('should return the rotated tetrimino if the rotation 2 is randomly chosen', async () => {
-			const randomIntStub = sandbox.stub(randomLib, 'getRandomInt').returns(2);
-
-			const res = await tetriminosLib.__TESTS__._getRandomRotation(fixtures.tetriminos().J);
-
-			expect(randomIntStub.callCount).to.equal(1);
-			expect(res).to.deep.equal({
-				shape: [
-					['J', 0, 0],
-					['J', 'J', 'J'],
-					[0, 0, 0],
-				],
-				color: '34, 118, 185',
-				rotationsPossible: 4,
-			});
-		});
-
 		it('should return the rotated tetrimino if the rotation 3 is randomly chosen', async () => {
 			const randomIntStub = sandbox.stub(randomLib, 'getRandomInt').returns(3);
 
@@ -79,24 +62,8 @@ describe('lib/tetriminos', () => {
 				rotationsPossible: 4,
 			});
 		});
-
-		it('should return the rotated tetrimino if the rotation 4 is randomly chosen', async () => {
-			const randomIntStub = sandbox.stub(randomLib, 'getRandomInt').returns(4);
-
-			const res = await tetriminosLib.__TESTS__._getRandomRotation(fixtures.tetriminos().J);
-
-			expect(randomIntStub.callCount).to.equal(1);
-			expect(res).to.deep.equal({
-				shape: [
-					[0, 0, 0],
-					['J', 'J', 'J'],
-					[0, 0, 'J'],
-				],
-				color: '34, 118, 185',
-				rotationsPossible: 4,
-			});
-		});
 	});
+
 	describe('#getRandomTetrimino()', () => {
 		it('should return the same tetrimino if there is only one rotation possible', async () => {
 			const indexBlockO = fixtures.tetriminos().BLOCK_NAMES.indexOf('O');
@@ -136,7 +103,7 @@ describe('lib/tetriminos', () => {
 		});
 	});
 
-	describe.skip('#createNewRandomTetriminos()', () => {
+	describe('#createNewRandomTetriminos()', () => {
 		it('should create one new random tetrimino and update the collection', async () => {
 			const randomTetriminoStub = sandbox
 				.stub(tetriminosLib, 'getRandomTetrimino')
@@ -153,19 +120,26 @@ describe('lib/tetriminos', () => {
 			expect(randomTetriminoStub.callCount).to.equal(1);
 			expect(updateBlocksListStub.args).to.deep.equal([[
 				'000000000000000000000001',
-				1,
-			]]);
-			expect(res).to.deep.equal({
-				matchedCount: 1,
-				modifiedCound: 1,
-				blockList: {
+				[{
 					shape: [
 						['O', 'O'],
 						['O', 'O'],
 					],
 					color: '255, 239, 53',
 					rotationsPossible: 1,
-				}
+				}]
+			]]);
+			expect(res).to.deep.equal({
+				matchedCount: 1,
+				modifiedCound: 1,
+				blockList: [{
+					shape: [
+						['O', 'O'],
+						['O', 'O'],
+					],
+					color: '255, 239, 53',
+					rotationsPossible: 1,
+				}]
 			});
 		});
 	})
