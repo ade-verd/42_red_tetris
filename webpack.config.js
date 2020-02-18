0d70c438ccbfca5299ea5e4baabf9410a363d8da
ADV <ade-verd@student.42.fr>
2020-02-18 15:11:31 +0100

    [ade-verd] fix package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
 var path = require('path');
 
 module.exports = {
-  entry: './src/client/index.js',
+    entry: './src/client/index.js',
 
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }, {
-				test: /\.css$/,
-				loader: "style-loader!css-loader"
-			}, {
-				test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-				loader: 'url-loader?limit=100000' }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader?limit=100000',
+            },
+        ],
+    },
 };
