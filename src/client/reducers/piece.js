6818b821ba5ac5a43c845efc24ad9831511ee79a
Onur OZKAYA <oozkaya@student.42.fr>
2020-02-26 14:20:09 +0100

    fixed random tetrimino call lib

--- src/client/reducers/piece.js
+++ src/client/reducers/piece.js
@@ -1,10 +1,6 @@
 import { FIELD_WIDTH } from '../helpers/fieldHelper';
 import { randomTetromino } from '../helpers/tetrominos';
-<<<<<<< HEAD:src/client/reducers/piece.js
 // const tetriminosLib = require('../../server/lib/tetriminos');
-=======
-const tetriminosLib = require('../../server/lib/tetriminos');
->>>>>>> 874e1fa16b3399184cf81430f4af7e3a8cce9729:src/client/reducers/piece.js
 
 const reducer = (state = {}, action) => {
     switch (action.type) {
@@ -12,12 +8,7 @@ const reducer = (state = {}, action) => {
 						console.log('STARTED PIECE REDUCER');
             return {
                 pos: { x: FIELD_WIDTH / 2 - 2, y: 0 },
-<<<<<<< HEAD:src/client/reducers/piece.js
 								tetromino: action.piece,
-								
-=======
-                tetromino: tetriminosLib.getRandomTetrimino().shape,
->>>>>>> 874e1fa16b3399184cf81430f4af7e3a8cce9729:src/client/reducers/piece.js
                 collided: false,
             };
         case 'rotate':
