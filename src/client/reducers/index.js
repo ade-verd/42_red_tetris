1e864f6ccaeb89607b0bca22937617d973b250c0
Onur OZKAYA <oozkaya@student.42.fr>
2020-02-27 14:07:07 +0100

    adding gameStatus

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,2 +1,13 @@
-import alert from './alert';
-export default alert;
+import alertReducer from './alert';
+import fieldReducer from './field';
+import gameStatusReducer from './gameStatus';
+import pieceReducer from './piece';
+import roomsReducer from './rooms';
+
+export default {
+	alertReducer,
+	fieldReducer,
+	gameStatusReducer,
+	pieceReducer,
+	roomsReducer
+};
