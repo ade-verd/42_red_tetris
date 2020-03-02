be10d558194ea1a43812c473d981e33da9c83eac
ADV <ade-verd@student.42.fr>
2020-03-02 13:23:26 +0100

    [ade-verd] rooms: display players ids on click

--- src/client/components/Rooms/DisplayRooms.js
+++ src/client/components/Rooms/DisplayRooms.js
@@ -1,22 +1,38 @@
 import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 
 import { MAX_PLAYERS } from '../../../constants';
 import './DisplayRooms.css';
 
+const buildCollapsedCard = (eventKey, playersNames) => {
+    if (!playersNames) return;
+
+    const formattedNames = playersNames.map(name => <div>{name}</div>);
+
+    return (
+        <Accordion.Collapse eventKey={eventKey}>
+            <div>{formattedNames}</div>
+        </Accordion.Collapse>
+    );
+};
+
 const Row = props => {
     const className = props.isTitle ? 'title-bar row-container' : 'row-container room';
+    const eventKey = `evt_${props.roomId}`;
     const nameValue = props.name || 'name';
-    const playersValue = props.playersNumber
-        ? `${props.playersNumber} / ${MAX_PLAYERS}`
-        : 'players';
+    const playersNames = props.players;
+    const playersNumber = props.players ? `${props.players.length} / ${MAX_PLAYERS}` : 'players';
     const statusValue = props.status || 'status';
 
     return (
-        <div className={className}>
-            <div className="item">{nameValue}</div>
-            <div className="item">{playersValue}</div>
-            <div className="item">{statusValue}</div>
-        </div>
+        <Accordion>
+            <Accordion.Toggle as="div" className={className} eventKey={eventKey}>
+                <div className="item">{nameValue}</div>
+                <div className="item">{playersNumber}</div>
+                <div className="item">{statusValue}</div>
+            </Accordion.Toggle>
+            {buildCollapsedCard(eventKey, playersNames)}
+        </Accordion>
     );
 };
 
@@ -29,10 +45,11 @@ const buildRoomsTable = activeRooms => {
     return activeRoomsArray.map(room => {
         return (
             <Row
-                key={room._id}
+                key={`row_${room._id}`}
+                roomId={room._id}
                 name={room.room_name}
                 status={room.game_status}
-                playersNumber={room.players_ids.length}
+                players={room.players_ids}
             />
         );
     });
