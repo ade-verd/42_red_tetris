e34ababee32697400ee198463d63bfc049ad1fde
ADV <ade-verd@student.42.fr>
2020-03-03 18:04:42 +0100

    [ade-verd] handle socket errors

--- src/client/components/Rooms/CreateRoom.js
+++ src/client/components/Rooms/CreateRoom.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 import { store } from '../../index';
 
+const roomCreationError = () => {
+    const state = store.getState();
+    const rooms = state.rms.rooms;
+
+    if (rooms && rooms.roomCreationError) {
+        const msg = rooms.roomCreationError;
+        return <div className="error">{msg}</div>;
+    }
+    return null;
+};
+
 const CreateRoom = props => {
     const onEnterkey = event => {
         if (event.key === 'Enter') {
@@ -16,6 +27,7 @@ const CreateRoom = props => {
         <div>
             <label>Create room:</label>
             <input type="text" id="name" onKeyDown={onEnterkey} required />
+            {roomCreationError()}
         </div>
     );
 };
--- src/client/reducers/room.js
+++ src/client/reducers/room.js
@@ -1,5 +1,15 @@
 'use strict';
 
+const handleError = (state, error, errorFieldName) => {
+    return {
+        ...state,
+        rooms: {
+            ...state.rooms,
+            [errorFieldName]: error,
+        },
+    };
+};
+
 const checkAndUpdatePlayersNames = (playerState, rooms, fnUpdatePlayers) => {
     if (!rooms) return;
 
@@ -19,15 +29,30 @@ const checkAndUpdatePlayersNames = (playerState, rooms, fnUpdatePlayers) => {
     });
 };
 
+const handleUpdateActiveRooms = (state, action) => {
+    if (action.error !== undefined) return handleError(state, action.error, 'updateRoomsError');
+
+    checkAndUpdatePlayersNames(state.players, action.rooms, action.fnUpdatePlayers);
+    return {
+        ...state,
+        rooms: action.rooms,
+        updateRoomsError: null,
+    };
+};
+
+const handleRoomCreation = (state, action) => {
+    if (action.error !== undefined) return handleError(state, action.error, 'roomCreationError');
+
+    return { ...state, roomCreationError: null };
+};
+
 const reducer = (state = {}, action) => {
     console.log('[roomReducer] State', state);
     switch (action.type) {
         case 'UPDATE_ACTIVE_ROOMS':
-            checkAndUpdatePlayersNames(state.players, action.rooms, action.fnUpdatePlayers);
-            return {
-                ...state,
-                rooms: action.rooms,
-            };
+            return handleUpdateActiveRooms(state, action);
+        case 'ROOM_CREATED':
+            return handleRoomCreation(state, action);
         default:
             return state;
     }
