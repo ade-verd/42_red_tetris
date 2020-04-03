4873ea0f7ecbce1d2daefca827c4de1474393c06
ade-verd <ade-verd@student.42.fr>
2020-04-03 15:50:38 +0200

    [ade-verd] nav tabs titles

--- src/client/components/ChatPanel/NavTabs/NavTabs.js
+++ src/client/components/ChatPanel/NavTabs/NavTabs.js
@@ -1,21 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tabs from 'react-bootstrap/Tabs';
+
+import { store } from '../../../index';
 
 import css from './NavTabs.module.css';
 
+const lobbyTab = () => {
+    return (
+        <div eventKey="lobby" title="Lobby">
+            Lobby
+        </div>
+    );
+};
+
+const roomTab = roomName => {
+    if (!roomName) return null;
+
+    return (
+        <div eventKey="room" title="My room">
+            {roomName}
+        </div>
+    );
+};
+
 const NavTabs = () => {
+    const [activeTab, setActiveTab] = useState('lobby');
+
+    const userState = store.getState().usr;
+    useEffect(() => {
+        userState.roomId ? setActiveTab('room') : setActiveTab('lobby');
+    }, [userState.roomId]);
+
     return (
         <div className={css['container']}>
-            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                <div eventKey="home" title="Home">
-                    Blablabla1
-                </div>
-                <div eventKey="profile" title="Profile">
-                    Blablabla2
-                </div>
-                <div eventKey="contact" title="Contact" disabled>
-                    Blablabla2
-                </div>
+            <Tabs activeKey={activeTab} onSelect={tab => setActiveTab(tab)} id="chat-panel">
+                {lobbyTab()}
+                {roomTab(userState.roomName)}
             </Tabs>
         </div>
     );
