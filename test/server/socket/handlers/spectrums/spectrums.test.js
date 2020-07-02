fb89799dce14f724bef1a42e4552585aae438c12
oozkaya <oozkaya@student.42.fr>
2020-07-02 21:27:45 +0200

    [oozkaya] gameStart & spectrums tests modified, still working on it, need to fix server io instance

--- test/server/socket/handlers/spectrums/spectrums.test.js
+++ test/server/socket/handlers/spectrums/spectrums.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
-const io = require('socket.io-client');
+const ioClt = require('socket.io-client');
+const { getIo } = require('../../../../../src/server/socket/index');
 
 const { startServer } = require('../../../../helpers/server');
 const config = require('../../../../../src/server/config');
@@ -10,23 +11,39 @@ const actionClient = require('../../../../../src/client/actions/game/spectrum');
 describe('socket/handlers/spectrums/spectrums', function() {
     const sandbox = sinon.createSandbox();
 
+    const ioSrv = getIo();
     const socketUrl = config.server.url;
     const options = {
         transports: ['websocket'],
         'force new connection': true,
     };
 
+    console.log('TEST', ioSrv, getIo)
+
     let server;
+    let client1;
+    let client2;
     before(cb => {
         startServer(config.server, function(err, srv) {
             if (err) throw err;
             server = srv;
             cb();
         });
+
+        const ROOM_ID = '000000000000000000000001';
+
+        ioSrv.on('connection', socket => {
+            socket.join(ROOM_ID);
+        });
+
+        client1 = ioClt.connect(socketUrl, options);
+        client2 = ioClt.connect(socketUrl, options);
     });
 
     after(done => {
         server.stop(done);
+        client1.disconnect();
+        client2.disconnect();
     });
 
     afterEach(() => {
@@ -34,10 +51,9 @@ describe('socket/handlers/spectrums/spectrums', function() {
     });
 
     it('should emit new updated spectrum', function(done) {
-        const client = io.connect(socketUrl, options);
-
         const ROOM_ID = '000000000000000000000001';
         const PLAYER_ID = '000000000000000000000002';
+        const PLAYER_NAME = 'PLAYER01'
         const FIELD = [
             [ [ 0, 'clear', false ], [ 0, 'clear', false ], [ 'l', 'merged', false ], [ 0, 'clear', false ] ],
             [ [ 0, 'clear', false ], [ 'l', 'merged', false ], [ 'l', 'merged', false ], [ 0, 'clear', false ] ],
@@ -51,35 +67,33 @@ describe('socket/handlers/spectrums/spectrums', function() {
             [ 'clear', 'merged', 'merged', 'clear' ],
         ];
 
-        client.emit(
+        client1.emit(
             'spectrum:update',
-            actionClient.getSpectrumPayload(ROOM_ID, PLAYER_ID, FIELD),
+            actionClient.getSpectrumPayload(ROOM_ID, PLAYER_ID, PLAYER_NAME, FIELD),
         );
-        client.on('tetriminos:get_random', payload => {
+        client2.on('spectrum:updated', payload => {
             expect(payload).to.deep.equal({
                 player_id: PLAYER_ID,
+                player_name: PLAYER_NAME,
                 spectrum: EXPECTED_SPECTRUM,
             });
-            client.disconnect();
             done();
         });
-    }).timeout(0);
+    });
 
     it('should not emit anything if an error occurs while updating spectrum', function(done) {
-        const client = io.connect(socketUrl, options);
-
         const ROOM_ID = '000000000000000000000001';
         const PLAYER_ID = '000000000000000000000002';
+        const PLAYER_NAME = 'PLAYER01';
         const FIELD = null;
 
-        client.emit(
+        client1.emit(
             'spectrum:update',
-            actionClient.getSpectrumPayload(ROOM_ID, PLAYER_ID, FIELD),
+            actionClient.getSpectrumPayload(ROOM_ID, PLAYER_ID, PLAYER_NAME, FIELD),
         );
-        client.on('tetriminos:get_random', payload => {
+        client2.on('spectrum:updated', payload => {
             expect(payload).to.deep.equal(null);
-            client.disconnect();
             done();
         });
-    }).timeout(0);
+    });
 });
