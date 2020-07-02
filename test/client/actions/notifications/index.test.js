1e79507b1412b45646cab738d532d63b4d4a0cd6
ADV <a.deverdilhac@gmail.com>
2020-07-02 15:51:46 +0200

    [ade-verd] coverage 100% client/actions/notifications

---
+++ test/client/actions/notifications/index.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import { expect } from 'chai';
+import { toast } from 'react-toastify';
+import sinon from 'sinon';
+
+import notify from '../../../../src/client/actions/notifications';
+
+describe('client/actions/notifications/index', () => {
+    const sandbox = sinon.createSandbox();
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+    it('should notify with info dialog', () => {
+        const MSG = 'message';
+        const TYPE = 'info';
+        const toastStub = sandbox.stub(toast, TYPE);
+
+        notify({ msg: MSG, type: TYPE });
+
+        expect(toastStub.args).to.deep.equal([['message']]);
+    });
+
+    it('should notify with success dialog', () => {
+        const MSG = 'message';
+        const TYPE = 'success';
+        const toastStub = sandbox.stub(toast, TYPE);
+
+        notify({ msg: MSG, type: TYPE });
+
+        expect(toastStub.args).to.deep.equal([['message']]);
+    });
+
+    it('should notify with warning dialog', () => {
+        const MSG = 'message';
+        const TYPE = 'warning';
+        const toastStub = sandbox.stub(toast, TYPE);
+
+        notify({ msg: MSG, type: TYPE });
+
+        expect(toastStub.args).to.deep.equal([['message', { autoClose: 5000 }]]);
+    });
+
+    it('should notify with error dialog', () => {
+        const MSG = 'message';
+        const TYPE = 'error';
+        const toastStub = sandbox.stub(toast, TYPE);
+
+        notify({ msg: MSG, type: TYPE });
+
+        expect(toastStub.args).to.deep.equal([['message', { autoClose: 6000 }]]);
+    });
+
+    it('should notify with error dialog by default', () => {
+        const MSG = 'message';
+        const TYPE = '';
+        const toastStub = sandbox.stub(toast, 'error');
+
+        notify({ msg: MSG, type: TYPE });
+
+        expect(toastStub.args).to.deep.equal([['message', { autoClose: 6000 }]]);
+    });
+});
