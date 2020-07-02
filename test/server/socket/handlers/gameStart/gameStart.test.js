2a60c0abd7d8652af7863c25cde095a0f178d4dc
oozkaya <oozkaya@student.42.fr>
2020-07-02 21:27:45 +0200

    [oozkaya] gameStart & spectrums tests modified, still working on it, need to fix server io instance

---
+++ test/server/socket/handlers/gameStart/gameStart.test.js
@@ -0,0 +1,104 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const ioClt = require('socket.io-client');
+const { getIo }  = require('../../../../../src/server/socket/index');
+
+const { startServer } = require('../../../../helpers/server');
+const config = require('../../../../../src/server/config');
+
+const actionClient = require('../../../../../src/client/actions/game/gameStart');
+
+describe('socket/handlers/gameStart/gameStart', function() {
+    const sandbox = sinon.createSandbox();
+
+    const ioSrv = getIo();
+    const socketUrl = config.server.url;
+    const options = {
+        transports: ['websocket'],
+        'force new connection': true,
+    };
+
+
+    let server;
+    let client1;
+    let client2;
+    before(cb => {
+        startServer(config.server, function(err, srv) {
+            if (err) throw err;
+            server = srv;
+            cb();
+        });
+
+        const ROOM_ID = '000000000000000000000001';
+        
+        ioSrv.on('connection', socket => {
+            socket.join(ROOM_ID);
+        });
+
+        client1 = ioClt.connect(socketUrl, options);
+        client2 = ioClt.connect(socketUrl, options);
+    });
+
+    after(done => {
+        server.stop(done);
+        client1.disconnect();
+        client2.disconnect();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should emit game start', function(done) {
+        const ROOM_ID = '000000000000000000000001';
+        const PIECES = [
+            { 
+                shape: [
+                    [0, 0, 0, 0],
+                    ['I', 'I', 'I', 'I'],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                ],
+                color: '29, 174, 236',
+                rotationsPossible: 2,
+            },
+            { 
+                shape: [
+                    [0, 0, "Z"],
+                    [0, "Z", "Z"],
+                    [0, "Z", 0],
+                ],
+                color: '234, 32, 45',
+                rotationsPossible: 2,
+            },
+        ];
+        const INDEX = 0;
+
+        client1.emit(
+            'game:start',
+            actionClient.getGameStartPayload(ROOM_ID, PIECES, INDEX),
+        );
+        client2.on('game:started', payload => {
+            expect(payload).to.deep.equal({
+                pieces: PIECES,
+                index: INDEX,
+            });
+            done();
+        });
+    });
+
+    it('should not emit anything if an error occurs while starting game', function(done) {
+        const ROOM_ID = '000000000000000000000001';
+        const PIECES = null;
+        const INDEX = 0;
+
+        client1.emit(
+            'game:start',
+            actionClient.getGameStartPayload(ROOM_ID, PIECES, INDEX),
+        );
+        client2.on('game:started', payload => {
+            expect(payload).to.deep.equal(null);
+            done();
+        });
+    });
+});
