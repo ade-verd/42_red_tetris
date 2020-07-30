849ac374d81059057943814f8e77444f58a1d06e
ADV <a.deverdilhac@gmail.com>
2020-07-30 14:33:05 +0200

    [ade-verd] change port env var

--- src/client/config/index.js
+++ src/client/config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     server: {
-        host: process.env.CONFIG_REDTETRIS_SERVER_HOST || '0.0.0.0',
-        port: process.env.CONFIG_REDTETRIS_SERVER_PORT || 3004,
+        host: process.env.CONFIG_SERVER_HOST || '0.0.0.0',
+        port: process.env.PORT || 3004,
         get url() {
             return 'http://' + this.host + ':' + this.port;
         },
--- src/server/config/index.js
+++ src/server/config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     server: {
         host: process.env.CONFIG_SERVER_HOST || '0.0.0.0',
-        port: process.env.CONFIG_SERVER_PORT || 3004,
+        port: process.env.PORT || 3004,
         get url() {
             return 'http://' + this.host + ':' + this.port;
         },
