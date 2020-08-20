a91f9820f79e00cdf43618b4a5e66ab41c9980bf
ADV <a.deverdilhac@gmail.com>
2020-08-20 11:35:13 +0200

    [ade-verd] add 404 page

--- src/client/components/Errors/E404_NotFound.js
+++ src/client/components/Errors/E404_NotFound.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import css from './Errors.module.css';
 
-export const E404_NotFound = ({}) => {
-    const ERROR_MESSAGE = 'Error 404: Not Found';
+const illustration = () => (
+    <div className={css.item}>
+        <img src="https://media.giphy.com/media/RjoLWhQBFEcHS/giphy.gif" alt="Lost Travolta" />
+    </div>
+);
+
+const errorInfo = () => {
+    const ERROR_TITLE = 'Error 404';
+    const ERROR_MESSAGE = 'It seems you are lost';
 
     return (
-        <div className={css.container}>
-            <div className={css.item}>{ERROR_MESSAGE}</div>;
-        </div>
+        <>
+            <div className={css.item}>
+                <p className={css.errorTitle}>{ERROR_TITLE}</p>
+                <p className={css.errorMessage}>{ERROR_MESSAGE}</p>
+            </div>
+        </>
     );
+};
+
+const redirectInfo = () => (
+    <div className={[css.item, css.redirect].join(' ')}>
+        {'You will be redirected in few seconds...'}
+    </div>
+);
+
+const buildError = () => (
+    <div className={css.container}>
+        {errorInfo()}
+        {illustration()}
+        {redirectInfo()}
+    </div>
+);
+
+const homeRedirection = () => <Redirect to="/" />;
+
+export const E404_NotFound = ({}) => {
+    const [content, setContent] = useState(buildError());
+
+    useEffect(() => {
+        setTimeout(() => setContent(homeRedirection()), 5000);
+    }, []);
+
+    return content;
 };
 
 export default E404_NotFound;
