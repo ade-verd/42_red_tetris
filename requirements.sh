1fbb965bafb8006bf232cfe09f47a69656d7086f
ADV <a.deverdilhac@gmail.com>
2020-08-24 17:12:32 +0200

    [ade-verd] update requirements file [skip ci]

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
-nvm install v12.13.1
-nvm use
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "Usage: source $(basename $0)"
+    exit 1
+fi
 
-npm install
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install v12.13.1 &&
+nvm use &&
+npm install &&
 npm run start-services
