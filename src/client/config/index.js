7e339409e80e524a7a869d2f0ba65f71ce4f44c5
ADV <a.deverdilhac@gmail.com>
2020-08-25 14:48:22 +0200

    [ade-verd] useless config [skip ci]

--- src/client/config/index.js
+++ src/client/config/index.js
@@ -8,40 +8,6 @@ export default {
             return 'http://' + this.host + ':' + this.port;
         },
     },
-    favicon: process.env.NODE_ENV === 'production' ? 'favicon.ico' : './assets/img/favicon.ico',
-    game: {
-        commands: [
-            {
-                key: '▲',
-                // key: '↑',
-                action: 'Rotate the piece',
-                key: 'ArrowUp',
-            },
-            {
-                key: '▼',
-                // key: '↓',
-                action: 'Move the piece to the bottom',
-                key: 'ArrowDown',
-            },
-            {
-                key: '◄',
-                // key: '←',
-                action: 'Move the piece to the left',
-                key: 'ArrowLeft',
-            },
-            {
-                key: '►',
-                // key: '→',
-                action: 'Move the piece to the right',
-                key: 'ArrowRight',
-            },
-            {
-                key: 'Space',
-                action: 'Drop the piece',
-                code: 'Space',
-            },
-        ],
-    },
     logger: {
         isReduxLoggerEnable: process.env.IS_ENABLE_REDUXLOGGER === 'true',
         level: process.env.LOG_LEVEL || 'trace',
